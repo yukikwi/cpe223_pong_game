4aa0d45370fcc42f3a4acdf4c5069c99b87840cb
IO <warn192444@gmail.com>
2020-12-04 14:45:30 +0700

    Update display_set_time.v

--- display_set_time.v
+++ display_set_time.v
@@ -26,9 +26,10 @@ module display_set_time (
     input [9:0] y,
     output display
     );
-    wire [7:0] text;
+    wire [6:0] text;
     wire [6:0] first_min, second_min, first_sec, second_sec;
     wire display_sec_first, display_sec_second, display_min;
+    reg dot;
     //SET SCORE
     char_s SetScore_s1(199, 140, x, y, text[0]);
     char_e SetScore_e1(230, 140, x, y, text[1]);
@@ -37,6 +38,13 @@ module display_set_time (
     char_i SetScore_i(354, 140, x, y, text[4]);
     char_m SetScore_m(385, 140, x, y, text[5]);
     char_e SetScore_e2(416, 140, x, y, text[6]);
+    
+    always @(x or y) begin
+        if(x >= 240 && x < 245 && ((y >= 150 && y < 155) || (y >= 165 && y < 170)))
+            dot = 1;
+        else
+            dot = 0;
+    end
     
     reg [3:0] min;
     reg [5:0] sec;
@@ -53,5 +61,5 @@ module display_set_time (
     display_seg FirstSec(first_sec, 286, 240, x, y, display_sec_first);
     display_seg SecondSec(second_sec, 317, 240, x, y, display_sec_second);
     
-    assign display = display_min | display_sec_first | display_sec_second | (|text);
+    assign display = display_min | display_sec_first | display_sec_second | (|text) | dot;
 endmodule
